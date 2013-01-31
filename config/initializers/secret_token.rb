211a65422d6e02a12eba20c895137e65bee13d72
Nico Catajoy <vnrcatajoy@gmail.com>
2013-01-31 22:46:14 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CparSystem::Application.config.secret_token = '701211a0655e81ef26759005f8125881a91b36d8805e46f41798cc25ff73ff3dc1101c7371d7b95fad360353e27433b21dbb2327f5f16934dde5d54b46bb8f23'
