31e5b3e70b6e9b5bb1abe026aa41e019114ae173
Homer Isidro <homer.isidro@gmail.com>
2013-02-19 11:09:22 +0800

    Fixed Activities showing under Action Plan

--- app/controllers/action_plans_controller.rb
+++ app/controllers/action_plans_controller.rb
@@ -31,5 +31,6 @@ class ActionPlansController < ApplicationController
 
   def show
   	@action_plan = ActionPlan.find params[:id]
+    @activities = @action_plan.activities.paginate(page: params[:page],  per_page: 5)
   end
 end
