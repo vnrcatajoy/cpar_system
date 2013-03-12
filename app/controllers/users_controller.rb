9e186fc88cc189716c92da351681b319bb78ace8
Homer Isidro <homer.isidro@gmail.com>
2013-03-12 18:32:22 +0800

    Remove destroy action from public User, add show to signed_in actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :show]
   before_filter :correct_user,   only: [:edit, :update]
-  before_filter :admin_user,     only: :destroy
 
   def new
   	@user = User.new
@@ -39,18 +38,5 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
-  end
-
-  private
-
-  def admin_user
-      redirect_to(root_path) unless current_user.admin?
-  end
-
 
 end
