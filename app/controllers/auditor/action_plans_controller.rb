d24a1a4c3e460951ad1755483b93dba78f6cf9f2
Homer Isidro <homer.isidro@gmail.com>
2013-03-14 19:54:14 +0800

    Fix ActionPlan index views, remove unnecessary ActionPlan controller actions from Auditor

--- app/controllers/auditor/action_plans_controller.rb
+++ app/controllers/auditor/action_plans_controller.rb
@@ -2,41 +2,9 @@ class Auditor::ActionPlansController < ApplicationController
   before_filter :signed_in_user
   before_filter :auditor_user
 
-  def new
-  	@action_plan = ActionPlan.new
-  end
-
-  def create
-  	@action_plan = ActionPlan.new(params[:action_plan])
-    @action_plan.responsible_officer_id = current_user.id
-    #since those who make action plans are responsible officers
-  	if @action_plan.save
-  		flash[:success] = "Successfully added Action Plan!" 
-  	    redirect_to auditor_action_plan_path(@action_plan)
-  	else
-  		flash.now[:error] = "Please fill in the fields properly." 
-  		render 'new'
-  	end
-  end
-
-  def edit
-  	@action_plan = ActionPlan.find params[:id]
-  end
-
   def index
     @action_plans = ActionPlan.paginate(page: params[:page],  per_page: 15)
   end
-
-  def update
-    @action_plan = ActionPlan.find params[:id]
-    if @action_plan.update_attributes(params[:action_plan])
-      flash[:success] = "Action Plan updated."
-      redirect_to auditor_action_plan_path
-    else
-      flash.now[:error] = "Please don't leave the fields blank."
-      render 'edit'
-    end
-  end 
 
   def review
     @action_plan = ActionPlan.find params[:id]
