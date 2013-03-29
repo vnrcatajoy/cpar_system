2f6c9dd617c1bce2f96e3b16ef4a1a2bae74690c
Homer Isidro <homer.isidro@gmail.com>
2013-03-29 19:18:52 +0800

    Minor update to Issue view, add back Edit Issue for User's own submitted Issue only

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,6 @@
 class IssuesController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :show, :new]
+  before_filter :signed_in_user, only: [:index, :show, :new, :edit]
+  before_filter :correct_submitter, only: [:edit]
   before_filter :correct_department, only: [:show]
 
   def index
@@ -9,6 +10,7 @@ class IssuesController < ApplicationController
     else  #get issues reported to you or your department
       @issues = Issue.where("department_id = " + current_user.department_id.to_s + " OR user_id = " + current_user.id.to_s).paginate(page: params[:page], per_page: 10)
     end
+      @issues_yours = Issue.where("user_id = " + current_user.id.to_s).paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -30,6 +32,21 @@ class IssuesController < ApplicationController
     @action_plans = ActionPlan.where("issue_id = " + @issue.id.to_s).paginate(page: params[:page], per_page: 5)
   end
 
+  def edit
+    @issue = Issue.find params[:id]
+  end
+
+  def update
+    issue = Issue.find params[:id]
+    if issue.update_attributes params[:issue]
+      flash[:success] = "The issue has been updated successfully!"
+      redirect_to issue_path  
+    else
+      flash[:error] = "There was an error in updating your issue."
+      redirect_to :back
+    end
+  end
+
   private
   def correct_department
     @issue = Issue.find params[:id]
@@ -37,12 +54,20 @@ class IssuesController < ApplicationController
       if !current_user.admin? 
       #remove this condition if admin mustn't be allowed to view all Issues in public
         if current_user.id == @issue.user_id
-          flash[:success] = "Viewing your submitted Issue."
+          #flash[:success] = "Viewing your submitted Issue."
         else
           flash[:error] = "You cannot view that Issue."
           redirect_to issues_path
         end
       end 
+    end
+  end
+
+  def correct_submitter
+    @issue = Issue.find params[:id]
+    if current_user.id != @issue.user_id 
+      flash[:error] = "You cannot Edit that Issue."
+      redirect_to issue_path
     end
   end
 
